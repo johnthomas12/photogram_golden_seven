24cf4817fa3ec7746693ddcd3c6afe4f272538bd
John Thomas <johnfrankthomas@gmail.com>
2015-10-27 17:00:53 -0500

    commit to master

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,42 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({:id => params[:id]})
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    #render("create_row.html.erb")
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    Photo.find_by({:id => params[:id]}).destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_row
+    @photo = Photo.find_by({:id => params[:id]})
+    @id = params[:id]
+  end
+
+  def update_row
+    p = Photo.find_by({:id => params[:id]})
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("http://localhost:3000/photos/#{p.id}")
+  end
+
 
 end
